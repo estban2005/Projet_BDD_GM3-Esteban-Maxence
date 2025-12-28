0bed1d44f90462945deec63194b8187d72a14956
estban2005 <esteban.mt2005@gmail.com>
2025-12-28 22:05:52 +0100

    Merge origin/master
    # Conflicts:
    #	src/main/java/com/mycompany/javafxb56/Lecture_fichier.java

--- src/main/java/fr/insa/toto/webui/MainLayout.java
+++ src/main/java/fr/insa/toto/webui/MainLayout.java
@@ -5,16 +5,18 @@ import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.page.Push;
 import fr.insa.toto.model.Utilisateur;
 import fr.insa.toto.webui.security.SessionInfo;
 
 /**
  * @author maxen
  */
+@Push // INDISPENSABLE pour que le chronomètre de VueTableMatch s'actualise en direct
 public class MainLayout extends AppLayout {
 
     public MainLayout() {
-        // 1. Menu latéral
+        // 1. Menu latéral (Drawer)
         this.addToDrawer(new MainMenu());
 
         // 2. Bouton Menu (Toggle)
@@ -35,14 +37,13 @@ public class MainLayout extends AppLayout {
         navbarContainer.setWidthFull();
         navbarContainer.setAlignItems(FlexComponent.Alignment.CENTER);
         
-        // Ta couleur Beige/Marron clair pour le haut
+        // Couleur Beige/Marron clair pour le haut
         navbarContainer.getStyle().set("background-color", "#F5F5DC");
         navbarContainer.getStyle().set("padding", "0 1em");
 
         this.addToNavbar(navbarContainer);
 
-        // 5. Correction de la couleur du tiroir (Drawer)
-        // On cible spécifiquement la partie "drawer" protégée par le Shadow DOM
+        // 5. Couleur du menu latéral (Drawer) via JavaScript (Shadow DOM)
         this.getElement().executeJs(
             "const drawer = this.shadowRoot.querySelector('[part=\"drawer\"]');" +
             "if (drawer) {" +
